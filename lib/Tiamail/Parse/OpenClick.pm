c89df90e75dd39d8af857253f2cc98978cb2980e
Jeremy Blain <cowboyatheart@gmail.com>
2013-03-12 01:11:30 -0400

    Sane license considering it's perl Initial parsing code - needs work

--- lib/Tiamail/Parse/OpenClick.pm
+++ lib/Tiamail/Parse/OpenClick.pm
@@ -3,6 +3,8 @@ package Tiamail::Parse::OpenClick;
 use strict;
 use warnings;
 
+use FileHandle;
+
 use base qw ( Tiamail::Parse );
 
 =doc
@@ -11,8 +13,35 @@ An open/click parser needs to parse log files and record opens and clicks.
 
 These will usually be via web server logs, GET /foo/bar/whatever
 
+Expects a file to parse
+
 =cut
 
+sub _init {
+	unless ($self->{args}->{file}) {
+		die "file is a required param";
+	}
+	unless (-f $self->{args}->{file}) {
+		die "file must be readable";
+	}
+	
+	$self->{reader} = FileHandle->new($self->{args}->{file}, 'r');
+
+};
+
+sub process_line {
+	my $self = shift;
+	my $line = shift;
+	if ($_ =~ m#GET /r/([^/]+)/([^/]+)/#) {
+		# looks sane for click 
+		$self->SUPER::_record_click($1 ,$2);
+		
+	}
+	elsif ($_ =~ m#GET /x.gif/([^/]+)/([^/]+)/#) {
+		# looks sane for open
+		$self->SUPER::_record_open($1, $2);
+	}
+}
 
 1;
 
