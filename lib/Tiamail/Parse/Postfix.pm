8d8b369bb814905b4cd6b76aac1f6cd473d28342
Jeremy Blain <cowboyatheart@gmail.com>
2013-05-22 18:07:17 -0400

    Use named params in postfix parser to be consistent with the open/click parser

--- lib/Tiamail/Parse/Postfix.pm
+++ lib/Tiamail/Parse/Postfix.pm
@@ -8,21 +8,22 @@ use base qw( Tiamail::Parse::File );
 # Mar 15 04:10:11 server postfix/smtp[2756]: 0B4797E2E: to=<to@example.com>, relay=gmail-smtp-in.l.google.com[74.125.142.27], delay=1, status=sent (250 2.0.0 OK 1363335073 fn5si1865101igc.56 - gsmtp)
 
 sub read_line {
-	my $lines = shift;
-	foreach ( @{$lines} ) {
-		chomp;
-		next if $_ =~ //;
-		next unless $_ =~ m#postfix/smtp.*?to=<([^>]+)>.*? status=(sent|bounce|deferred|expired)/#;
+	my $self = shift;
+	my $line = shift;
+	
+	# Mar 15 04:10:11 server postfix/smtp[2756]: 0B4797E2E: to=<to@example.com>, relay=gmail-smtp-in.l.google.com[74.125.142.27], delay=1, status=sent (250 2.0.0 OK 1363335073 fn5si1865101igc.56 - gsmtp)
+
+
+	return unless $line =~ m#postfix/smtp.*?to=<([^>]+)>.*? status=(sent|bounce|deferred|expired)/#;
 		
-		if ($2 eq 'bounce' || $2 eq 'expired') {
-			record_email_hard_bounce($1);
-		}
-		elsif ($2 eq 'deferred') {
-			record_email_soft_bounce($1);
-		}
-		elsif ($2 eq 'sent') {
-			record_email_success($1);
-		}
+	if ($2 eq 'bounce' || $2 eq 'expired') {
+		$self->record_email_hard_bounce($1);
+	}
+	elsif ($2 eq 'deferred') {
+		$self->record_email_soft_bounce($1);
+	}
+	elsif ($2 eq 'sent') {
+		$self->record_email_success($1);
 	}
 }
 
