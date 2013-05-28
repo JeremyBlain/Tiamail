bde06b6f2b970c5d73bf2d75590d1be1be5a5265
Jeremy Blain <cowboyatheart@gmail.com>
2013-05-28 14:00:58 -0400

    Optional link parameter

--- lib/Tiamail/Parse/OpenClick.pm
+++ lib/Tiamail/Parse/OpenClick.pm
@@ -11,13 +11,14 @@ sub read_line {
 
 	# "-" - - [21/May/2013:17:26:42 -0400] "GET /r/10/1000001/http://www.google.ca/ HTTP/1.1" 302 245 "-" "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.68 Safari/537.17"
 
-	return unless $line =~ m#^"(\-|[\d\.\, ]+)".*?GET /(r|x.gif)/([A-Za-z0-9_]+)/([A-Za-z0-9_]+)#;
+	return unless $line =~ m#^"(\-|[\d\.\, ]+)".*?GET /(r|x.gif)/([A-Za-z0-9_]+)/([A-Za-z0-9_]+)(\/?.*?)$#;
 	if ($2 eq 'r') {
 		# record click
 		$self->record_email_click(
 			id => $3,
 			template_id => $4,
-			ip => $1
+			ip => $1,
+			link => $5,
 		);
 	}
 	else {
