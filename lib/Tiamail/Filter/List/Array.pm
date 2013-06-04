9303270e6eef2d960fdaa9200cbc509dfd993e8b
Jeremy Blain <cowboyatheart@gmail.com>
2013-06-04 18:15:23 -0400

    Rework Filter/List/Array.pm to play nicer with persistence.

--- lib/Tiamail/Filter/List/Array.pm
+++ lib/Tiamail/Filter/List/Array.pm
@@ -5,26 +5,33 @@ use warnings;
 
 use base qw( Tiamail::Filter );
 
+sub init {
+	if (!$self->{_init}) {
+		unless ($self->{args}->{list} && UNIVERSAL::isa($self->{args}->{list}, 'ARRAY')) {
+			die "Must specify a list argument to new, which must be an array ref";
+		}
+		my %hash = map { lc($_) => 1 } @{ $self->{args}->{list} };
+		$self->{hash} = \%hash;
+		$self->{_init} = 1;
+	}
+	return 1;
+}
+
 sub filter {
 	my $self = shift;
 	my $list = shift;
 	unless ($list && UNIVERSAL::isa($list, 'ARRAY')) {
 		die "filter expects a an array list as an argument";
 	}
-	unless ($self->{args}->{list} && UNIVERSAL::isa($list, 'ARRAY')) {
-		die "Must specify a list argument to new, which must be an array ref";
-	}
 
-	# map the list to a hash for faster lookups
-	my %hash = map { lc($_) => 1 } @{ $self->{args}->{list} };
-
+	$self->init();
 
 	# this should be able to be made more memory efficient not dereferencing list
 	# and making a second copy
 	my @new_list = ();
 	foreach my $entry (@{ $list }) {
 		# if the entry does not appear in our filter list, add it to the new list.
-		if (!exists( $hash{ lc($entry->{ $self->{args}->{field} }) }) ) {
+		if (!exists( $self->{hash}->{ lc($entry->{ $self->{args}->{field} }) }) ) {
 			push(@new_list, $entry);
 		}
 	}
