0d38fb85fd214bbe6c5fd05c33727d422b3d3c27
Jeremy Blain <cowboyatheart@gmail.com>
2013-06-05 12:44:33 -0400

    Start test cases

---
+++ t/Tiamail/Filter/File.pm
@@ -0,0 +1,20 @@
+package Tiamail::Test::Filter::File;
+
+use strict;
+use warnings;
+
+use base qw( Test::Unit::TestCase );
+
+sub new {
+	my $self = shift()->SUPER::new(@_);
+        return $self;
+}
+
+sub set_up {
+}
+
+sub tear_down {
+}
+
+
+1;
