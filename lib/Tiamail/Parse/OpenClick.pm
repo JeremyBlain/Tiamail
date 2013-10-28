e36f28a61b04d44e083c092dc408400ebbaa9195
Jeremy Blain <cowboyatheart@gmail.com>
2013-10-28 14:58:03 -0400

    Adjust parser to use the time in the log file not NOW()

--- lib/Tiamail/Parse/OpenClick.pm
+++ lib/Tiamail/Parse/OpenClick.pm
@@ -5,18 +5,27 @@ use warnings;
 
 use base qw( Tiamail::Parse::File );
 
+use Date::Manip;
+
 sub read_line {
 	my $self = shift;
 	my $line = shift;
 
 	# "-" - - [21/May/2013:17:26:42 -0400] "GET /r/10/1000001/http://www.google.ca/ HTTP/1.1" 302 245 "-" "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.68 Safari/537.17"
 
-	return unless $line =~ m#^"(\-|[\d\.\, ]+)".*?GET /(r|x.gif)/([A-Za-z0-9_]+)/([A-Za-z0-9_]+)(\/?.*?)$#;
-	if ($2 eq 'r') {
-		my $id = $3;
-		my $template_id = $4;
-		my $ip = $1;
-		my $link = $5;
+	return unless $line =~ m#^"(\-|[\d\.\, ]+)"\s+\-\s+\-\s+\[(\d+\/[A-Za-z]+\/\d+:\d+:\d+:\d+\s+[\+\-]\d+)\]\s+\"GET /(r|x.gif)/([A-Za-z0-9_]+)/([A-Za-z0-9_]+)(\/?.*?)$#;
+
+#	return unless $line =~ m#^"(\-|[\d\.\, ]+)"\s.*?GET /(r|x.gif)/([A-Za-z0-9_]+)/([A-Za-z0-9_]+)(\/?.*?)$#;
+
+	my $type = $3;
+	my $id = $4;
+	my $template_id = $5;
+	my $ip = $1;
+	my $link = $6;
+	my $time = generate_time($2);
+	$link =~ s/^\///g;
+
+	if ($type eq 'r') {
 		$link =~ s/^\///g;
 		# record click
 		$self->record_email_click(
@@ -24,16 +33,23 @@ sub read_line {
 			template_id => $template_id,
 			ip => $ip,
 			link => $link,
+			time => $time
 		);
 	}
 	else {
 		# record open
 		$self->record_email_open(
-			id => $3,
-			template_id => $4,
-			ip => $1
+			id => $id,
+			template_id => $template_id,
+			ip => $ip,
+			time => $time
 		);
 	}
 }
-
+sub generate_time {
+	my $string = shift;
+	my $date = new Date::Manip::Date;
+	my $err = $date->parse_format('%d/%b/%Y:%T %z',$string);
+	return $date->secs_since_1970_GMT();
+}
 1;
