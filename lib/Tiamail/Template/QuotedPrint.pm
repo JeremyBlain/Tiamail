48f8a65951ab4414fef335d6d8edd7555c300e26
Jeremy Blain <cowboyatheart@gmail.com>
2014-01-09 17:02:52 -0500

    Add QuotedPrint templater.
    Eventually should rework the whole templater to be more Email::MIME style especially the headers.
    Courriel by DROLSKY seems like it would be ideal to base the work around.

---
+++ lib/Tiamail/Template/QuotedPrint.pm
@@ -0,0 +1,49 @@
+package Tiamail::Template::QuotedPrint;
+
+use MIME::QuotedPrint;
+use Email::Date::Format qw( email_date );
+use Email::MessageID;
+
+use strict;
+use warnings;
+
+use base qw( Tiamail::Template::File );
+
+sub render {
+	my ($self, $params) = @_;
+
+	my $body = $self->{body};
+	my $headers = $self->{headers};
+
+	# some sanity cleanup after headers
+	$headers =~ s/[\r\n]+$//;
+
+	$headers = $self->_search_replace($headers, $params);
+	$body = $self->_search_replace($body, $params);
+	if ($self->{tracking}) {
+		$body = $self->_add_tracking($body, $params->{$self->{id}}, $self->{template_id}, $self->{base_url});
+	}
+
+	# sanitize headers
+	# first strip out \r
+	$headers =~ s/\r//;
+
+	# strip out encoding header
+	$headers =~ s/^Content-Transfer-Encoding:.*?$//mi;
+	$headers =~ s/^Date:.*?$//mi;
+	$headers =~ s/^Message-Id:.*?//mi;
+
+	$headers =~ s/\n\n/\n/g;
+
+	$headers .= "\nMessage-ID: " . Email::MessageID->new->in_brackets;
+
+	$headers .= "\nDate: " . email_date();
+
+	$headers .= "\nContent-Transfer-Encoding: quoted-printable";
+
+	# fix up \r\n for headers
+	$headers =~ s/\n/\r\n/g;
+
+	return $headers . "\r\n\r\n" . encode_qp($body, "\r\n");
+}
+1;
